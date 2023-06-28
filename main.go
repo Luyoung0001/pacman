53c24643c6705c2615e6c3585ed624a98aa26c2b
Luyoung0001 <luyoung0001@gmail.com>
2023-06-28 21:20:43 +0800

    a fs

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
-package pacman
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	// 加载静态文件
+	r.Static("/statics", "/Users/luliang/GoLand/pacman/statics")
+	r.LoadHTMLGlob("/Users/luliang/GoLand/pacman/statics/*")
+
+	r.GET("/home", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.Run(":9092")
+}
